9e3fab58c8bbb8e81d7376f2ef4f0cee1c488d04
urusa7 <abt737@gmail.com>
2017-08-24 16:28:48 -0700

    renamed foo to FOO

--- app/src/main/java/com/example/urusa7/stack2/Home.java
+++ app/src/main/java/com/example/urusa7/stack2/Home.java
@@ -3,11 +3,11 @@ package com.example.urusa7.stack2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/com/example/urusa7/stack2/Login.java
@@ -0,0 +1,50 @@
+package com.example.urusa7.stack2;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+EditText mEmail, mPassword;
+    TextView mforgotpassword;
+    Button mLogin, mRegister, MGooglelogin;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+    FirebaseUser mUser;
+    public static final String TAG = "Login";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mEmail = (EditText) findViewById(R.id.login_emailId);
+        mPassword = (EditText) findViewById(R.id.login_password);
+        mforgotpassword = (TextView) findViewById(R.id.forgotPassword);
+        mLogin = (Button) findViewById(R.id.login_here);
+        mRegister = (Button) findViewById(R.id.register_here);
+        MGooglelogin = (Button) findViewById(R.id.google_login);
+         mAuth = FirebaseAuth.getInstance();
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public  void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (mUser!=null) {
+                    Intent intent = new Intent(Login.this,Home.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else {
+                    Log.d(TAG,"AuthStateChange:LogOut");
+                }
+            }
+        };
+
+    }
+}
